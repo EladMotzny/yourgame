96e3bf5314e0d32709de70a3b44703c5027c5ccb
Anna <33619352+annty2@users.noreply.github.com>
2020-05-24 16:51:37 +0300

    fix bugs with flipping and optimize code

--- Blast/Assets/Scripts/CharacterMove.cs
+++ Blast/Assets/Scripts/CharacterMove.cs
@@ -9,40 +9,52 @@ public class CharacterMove : MonoBehaviour
     [Tooltip("Change to increase/decrease movement speed")] [SerializeField] float moveSpeed = 4f;
     [Tooltip("Change to control left movement")] [SerializeField] KeyCode leftKey;
     [Tooltip("Change to control right movement")] [SerializeField] KeyCode rightKey;
-    [Tooltip("Change to control left movement")] [SerializeField] KeyCode leftKey2;
-    [Tooltip("Change to control right movement")] [SerializeField] KeyCode rightKey2;
     private bool direction; //false- right, true- left
 
 
     // Start is called before the first frame update
     void Start()
     {
-        direction = true;
+        direction = true;// the charcters always look at the left side at the beggining
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(leftKey) || Input.GetKey(leftKey2))
+        if (Input.GetKey(leftKey)) //going left- both characters
         {
-            if (!direction)
+            if (!getDirection())//check if flipping is needed
             {
-                transform.rotation = Quaternion.Euler(0, 180, 0);
+                transform.rotation = Quaternion.Euler(0, 0, 0);//face left
+                setDirection(true);
             }
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+
             Vector3 movement = new Vector3(-1, 0, 0);
-            transform.position += movement * Time.deltaTime * moveSpeed;
+            transform.position += movement * Time.deltaTime * moveSpeed;//move
         }
-        if (Input.GetKey(rightKey) || Input.GetKey(rightKey2))
+        if (Input.GetKey(rightKey))//going right- both charachters
         {
-            if (direction)
+            if (getDirection())//check if flipping is needed
             {
-                transform.rotation = Quaternion.Euler(0, 180, 0);
+                transform.rotation = Quaternion.Euler(0, 180, 0);//face right
+                setDirection(false);
             }
-            transform.rotation = Quaternion.Euler(0, 180, 0);
 
             Vector3 movement = new Vector3(1, 0, 0);
-            transform.position += movement * Time.deltaTime * moveSpeed;
+            transform.position += movement * Time.deltaTime * moveSpeed;//move
         }
+    }
+
+
+    public bool getDirection()
+    {
+        Debug.Log("the direction is " + this.direction);
+        return this.direction;
+        
+    }
+
+    public void setDirection(bool newDirection)
+    {
+        this.direction = newDirection;
     }
 }
