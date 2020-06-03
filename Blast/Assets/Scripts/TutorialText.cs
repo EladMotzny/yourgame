e59f80a192091946b3d9ba78c49ef7b773204146
Elad Motzny <emotzny@gmail.com>
2020-06-03 16:00:47 +0300

    Made the text and prefab spawn for the tutorial

--- Blast/Assets/Scripts/TutorialText.cs
+++ Blast/Assets/Scripts/TutorialText.cs
@@ -9,20 +9,23 @@ public class TutorialText : MonoBehaviour
 {
     public TextMeshProUGUI textDisplay;
     public string[] sentences;
-    public Transform[] prefabs;
+    public GameObject[] prefabs;
     int clickCount = 0;
     [Tooltip("Typing speed")] [SerializeField] float typeSpeed = 0.02f;
     public GameObject continueButton;
+    GameObject instance;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Type());
+        instance = Instantiate(prefabs[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (textDisplay.text == sentences[clickCount])//if true, sentence typing is done
         {
             continueButton.SetActive(true);//show the button again
@@ -31,6 +34,7 @@ public class TutorialText : MonoBehaviour
 
     IEnumerator Type()//Type the first sentence in the textbox above booba
     {
+        //isSpawned = true;
         foreach (char letter in sentences[clickCount].ToCharArray())
         {
             textDisplay.text += letter;
@@ -41,8 +45,11 @@ public class TutorialText : MonoBehaviour
 
     public void NextSentence()//type the next sentence after clicking continue
     {
+        
+        spawnPrefab(clickCount);
 
-        //continueButton.SetActive(false);//hide button while sentence is being typed
+
+        continueButton.SetActive(false);//hide button while sentence is being typed
 
         if (clickCount < sentences.Length - 1)//check if the sentence is done
         {
@@ -54,6 +61,37 @@ public class TutorialText : MonoBehaviour
         {
             textDisplay.text = "";
             //continueButton.SetActive(false);
+        }
+    }
+    private void spawnPrefab(int clickCount)
+    {
+        switch (clickCount)
+        {
+            
+            case 1:
+                Debug.Log("Load ballBlue");
+                Destroy(instance);
+                instance = Instantiate(prefabs[1]);
+                break;
+            case 2:
+                Debug.Log("Load CharControl");
+                Destroy(instance);
+                instance = Instantiate(prefabs[2]);
+                break;
+            case 4:
+                Debug.Log("Load Against");
+                Destroy(instance);
+                instance = Instantiate(prefabs[3]);
+                break;
+            case 5:
+                Debug.Log("Load Wall");
+                Destroy(instance);
+                instance = Instantiate(prefabs[4]);
+                break;
+            case 8:
+                Debug.Log("Load scene");
+                SceneManager.LoadScene("Welcome");
+                break;
         }
     }
 }
