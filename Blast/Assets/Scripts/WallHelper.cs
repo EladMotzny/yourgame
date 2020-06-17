adc3c13bfde35053e06c5b6fcfcc269ca97ba79f
Elad Motzny <emotzny@gmail.com>
2020-06-17 19:47:40 +0300

    Added moving wall functionality

---
+++ Blast/Assets/Scripts/WallHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHelper : MonoBehaviour
+{
+    //add transform for the two walls
+    [Tooltip("Change to increase/decrease movement speed")] [SerializeField] float moveSpeed = 1f;
+    [Tooltip("Maximum delay for walls to open")] [SerializeField] int openMax = 10;
+    [Tooltip("Minimum delay for walls to open")] [SerializeField] int openMin = 3;
+    [Tooltip("Maximum delay for walls to close")] [SerializeField] int closeMax = 10;
+    [Tooltip("Maximum delay for walls to close")] [SerializeField] int closeMin = 3;
+    private System.Random rnd = new System.Random();
+
+
+    private bool shouldLerp = false;
+    public float timeStartedLerping;
+    public float lerpTime;
+    [Tooltip("The start coordinates for the upper wall")] [SerializeField] public Vector2 startPositionUpWall;
+    [Tooltip("The end coordinates for the upper wall")] [SerializeField] public Vector2[] endPositionUpWall;//3 positions
+    [Tooltip("The start coordinates for the down wall")] [SerializeField] public Vector2 startPositionDownWall;
+    [Tooltip("The end coordinates for the down wall")] [SerializeField] public Vector2[] endPositionDownWall;//3 positions
+    public WallMover uppperWall;
+    public WallMover downWall;
+    //public Transform downWall;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(openWall());
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator openWall()//function to open the wall after a certain amount of time
+    {
+        float sec = (float)rnd.Next(openMin, openMax);
+        //call function to open wall here
+        int position = rnd.Next(0, 2);
+        WallMover.pos = position;
+        uppperWall.StartLerping();
+        downWall.StartLerping();
+        Debug.Log("OPEN Wait for " + sec + " seconds");
+        yield return new WaitForSeconds(sec);
+        StartCoroutine(closeWall());
+    }
+
+    public IEnumerator closeWall()
+    {
+        float sec = (float)rnd.Next(closeMin, closeMax);
+        //call function to close wall here
+        WallMover.pos = 3;
+        uppperWall.StartLerping();
+        downWall.StartLerping();
+        Debug.Log("CLOSE Wait for " + sec + " seconds");
+        yield return new WaitForSeconds(sec);
+        StartCoroutine(openWall());
+    }
+
+}
--- Blast/Assets/Scripts/WallMover.cs
+++ Blast/Assets/Scripts/WallMover.cs
@@ -1,20 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WallMover : MonoBehaviour
 {
 
-    [Tooltip("Change to increase/decrease movement speed")] [SerializeField] float moveSpeed = 1f; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool shouldLerp = false;
+    public float timeStartedLerping;
+    public float lerpTime;
+    [Tooltip("The start coordinates for the upper wall")] [SerializeField] public Vector2 startPositionWall;
+    [Tooltip("The end coordinates for the upper wall")] [SerializeField] public Vector2[] endPositionWall;//3 positions
+    public static int pos;
 
     // Update is called once per frame
     void Update()
     {
         
+        if (shouldLerp)
+        {
+            switch (pos)
+            {
+                case 0://first position
+                    transform.position = LerpTest(startPositionWall, endPositionWall[0], timeStartedLerping, lerpTime);
+                    break;
+                case 1://second position
+                    transform.position = LerpTest(startPositionWall, endPositionWall[1], timeStartedLerping, lerpTime);
+                    break;
+                case 2://third position
+                    transform.position = LerpTest(startPositionWall, endPositionWall[2], timeStartedLerping, lerpTime);
+                    break;
+                case 3://close
+                    transform.position = LerpTest(transform.position, startPositionWall, timeStartedLerping, lerpTime);
+                    break;
+            }
+                
+
+            
+        }
     }
+
+
+    public void StartLerping()
+    {
+        timeStartedLerping = Time.time;
+        shouldLerp = true;
+    }
+
+    public Vector3 LerpTest(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 1)
+    {
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+        var result = Vector3.Lerp(start, end, percentageComplete);
+        return result;
+    }
+
 }
